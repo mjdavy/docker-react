53fdad258caf8f7b570f6f8309e8786b92f276e1
Martin Davy <martin.davy@gmail.com>
2020-10-16 12:21:00 -0400

    added new feature

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Martin Davy - That is fucking cool Woo!
+          Hello Martin Davy - That is fucking cool feature!
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
